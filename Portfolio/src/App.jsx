da1bd24c6d9fe8a35119a534438b2c5c5f80802e
Sharon Mishra <sharonmishra4@gmail.com>
2025-06-22 01:42:27 +0530

    Add OurWork and OurVision sections

---
+++ Portfolio/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import OurWork from './components/OurWork';
+import OurVision from './components/OurVision';
+
+function App() {
+  return (
+    <div>
+      <OurWork />
+      <OurVision />
+    </div>
+  );
+}
+
+export default App;
